a809b76aa5501d0744d7d2da82ff752ff9b51c7c
Michael Salaverry <michaels@wix.com>
2018-07-11 18:46:12 +0300

    chunk with a little help from the videos. I hope you like my jsdocs

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -1,13 +1,40 @@
-// --- Directions
-// Given an array and chunk size, divide the array into many subarrays
-// where each subarray is of length size
-// --- Examples
-// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
-// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
-// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
-// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
-// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-
-function chunk(array, size) {}
+/** 
+ * # Chunk
+ * 
+ * Given an array and chunk size, divide the array into many subarrays
+ * where each subarray is of length size
+ * 
+ * ## Examples
+ * 
+ * ```javascript
+   chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+   ```
+ * ```javascript
+   chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+   ```
+ * ```javascript
+   chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+   ```
+ * ```javascript
+   chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+   ```
+ * ```javascript
+   chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+   ```
+ * @param {number[]} array 
+ * @param {number} size 
+ */
+function chunk(array, size) {
+    const chunked = [];
+    for(let number of array) {
+        const tail = chunked[chunked.length - 1];
+        if (typeof tail === 'undefined' || tail.length >= size) {
+            chunked.push([number]);
+        } else {
+            tail.push(number);
+        }
+    }
+    return chunked;
+}
 
 module.exports = chunk;
