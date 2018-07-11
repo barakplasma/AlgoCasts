a1b8dfccd29f3d841ec3b0ab62937907e65cfac0
Michael Salaverry <michaels@wix.com>
2018-07-11 16:41:34 +0300

    used Map as Iterable obj to find maxChar of a given string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+/**
+ *
+ * @param {Map<string, number>} prevMap
+ * @param {string} currChar
+ */
+function addToCharMap(prevMap, currChar) {
+  const prevMapValue = prevMap.get(currChar) || 0;
+  prevMap.set(currChar, prevMapValue + 1);
+  return prevMap;
+}
+
+/**
+ *
+ * @param {string[]} charArr
+ * @returns {Map<string, number>}
+ */
+function generateCharMap(charArr) {
+  return charArr.reduce(addToCharMap, new Map());
+}
+
+function maxChar(str) {
+  const charArr = str.split("");
+  const charMap = generateCharMap(charArr);
+
+  let mostCommonChar = ['0', 0];
+  for (let char of charMap.entries()) {
+    if (char[1] > mostCommonChar[1]) {
+      mostCommonChar = char;
+    }
+  }
+  return mostCommonChar[0];
+}
 
 module.exports = maxChar;
